53cd2a7de8141b50ac75a00fe9c68a531b9f498c
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-07-20 12:14:22 +0530

    seconend coomit

--- courseSchedule.java
+++ courseSchedule.java
@@ -1,4 +1,11 @@
-class Solution {
+// There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+
+ class Solution {
     public boolean dfs(int node, List<List<Integer>> adj, boolean[] visit, boolean[] inStack) {
         // If the node is already in the stack, we have a cycle.
         if (inStack[node]) {
