befc0602996b16c9b298548a6420717f73f89bfd
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-08-05 05:44:27 +0530

    practice of class

---
+++ cal.js
@@ -0,0 +1,35 @@
+//cleass inhertahe 
+// //const number =[1, 2, 3]
+
+// // const doublenum = number.map((num) => {
+// //     return num*2;
+// // });
+
+// // console.log(number);
+
+// // console.log(doublenum);
+
+// class Human{
+//     constructor() {
+//         this.gender = 'human';
+//     }
+//     printgender() {
+//         console.log(this.gender);
+// }
+// }
+
+
+// class Person extends Human{
+//     constructor () { 
+//         super();
+//         this.name ="John";
+
+// }
+// printMyName() {
+//     console.log(this.name);
+// }
+// }
+
+// const person = new Person();
+// person.printMyName();
+// person.printgender();
