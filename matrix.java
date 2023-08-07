44d27e74868fd4652e4ce8ea5badf72dfb8f8d8a
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-08-07 16:49:25 +0530

    Update matrix.java

--- matrix.java
+++ matrix.java
@@ -1,3 +1,10 @@
+/*
+Each row is sorted in non-decreasing order.
+The first integer of each row is greater than the last integer of the previous row.
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+Output: false
+*/
+
 class Solution {
      public boolean searchMatrix(int[][] matrix, int target) {
             int i = 0, j = matrix[0].length - 1;
