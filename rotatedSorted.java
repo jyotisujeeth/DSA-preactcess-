c115132b1379a2fb49788872662d3b09ac4e3f01
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-08-10 19:38:59 +0530

    Update rotatedSorted.java

--- rotatedSorted.java
+++ rotatedSorted.java
@@ -63,3 +63,40 @@ class Solution {
         return arr[start] <= element;
     }
 }
+//////////////////////////////////////////////////////////////////////
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        """
+        Cases to consider:
+        4,1 target 4
+        4,1 target 1
+        1,4 target 4
+        1,4 target 1
+        """
+        n = len(nums)
+        left = 0
+        right = n-1
+        while left < right:
+            mid = left + (right - left) // 2
+            while left < mid and nums[left] == nums[mid]:
+                left += 1
+                mid = left + (right - left) // 2
+            
+            while mid < right and nums[mid] == nums[right]:
+                right -= 1
+                mid = left + (right - left) // 2
+
+            if nums[mid] == target: return True
+            elif nums[mid] < nums[right]:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid
+            else:
+                if nums[left] <= target and target <= nums[mid]:
+                    right = mid
+                else:
+                    left = mid + 1
+
+        if nums[left] == target: return True
+        else: return False
