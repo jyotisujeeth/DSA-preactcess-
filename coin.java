c0c5fecd42e604b57d7c06b14c44195bf6b149ec
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-08-11 11:28:31 +0530

    Create coin.java

---
+++ coin.java
@@ -0,0 +1,44 @@
+//Coin Change 
+/*
+Input: amount = 5, coins = [1,2,5]
+Output: 4
+Explanation: there are four ways to make up the amount:
+5=5
+5=2+2+1
+5=2+1+1+1
+5=1+1+1+1+1
+*/
+
+class Solution {
+    int[][] memo;
+    int[] coins;
+
+    public int numberOfWays(int i, int amount) {
+        if (amount == 0) {
+            return 1;
+        }
+        if (i == coins.length) {
+            return 0;
+        }
+        if (memo[i][amount] != -1) {
+            return memo[i][amount];
+        }
+
+        if (coins[i] > amount) {
+            return memo[i][amount] = numberOfWays(i + 1, amount);
+        }
+
+        memo[i][amount] = numberOfWays(i, amount - coins[i]) + numberOfWays(i + 1, amount);
+        return memo[i][amount];
+    }
+
+    public int change(int amount, int[] coins) {
+        this.coins = coins;
+        memo = new int[coins.length][amount + 1];
+        for (int[] row : memo) {
+            Arrays.fill(row, -1);
+        }
+
+        return numberOfWays(0, amount);
+    }
+}
